9590e847958d628aa2f1337ab4ed8bb17ed55df9
gxk <liang.lu@duitang.com>
2020-02-17 12:23:58 +0800

    :sparkles: +-*/

---
+++ src/main/java/com/gxk/enk/domain/expression/Addition.java
@@ -0,0 +1,35 @@
+package com.gxk.enk.domain.expression;
+
+import com.gxk.enk.domain.expression.Expression;
+import com.gxk.enk.gen.ExpressionGenerator;
+
+public class Addition extends Expression {
+
+  private final int type;
+  private final Expression left;
+  private final Expression right;
+
+  public Addition(Expression leftExp, Expression rightExp) {
+    type = leftExp.getType();
+    this.left = leftExp;
+    this.right = rightExp;
+  }
+
+  @Override
+  public void accept(ExpressionGenerator generator) {
+    generator.gen(this);
+  }
+
+  @Override
+  public int getType() {
+    return type;
+  }
+
+  public Expression getLeft() {
+    return left;
+  }
+
+  public Expression getRight() {
+    return right;
+  }
+}
